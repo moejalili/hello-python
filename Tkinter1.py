9d7c66abbec6bf66fbabf905f25293c707a60085
Marco Jalili <smhj63@gmail.com>
2020-04-11 17:33:08 +1000

    Testing Tkinter code: Tkinter is a Python binding to the Tk GUI toolkit

---
+++ Tkinter1.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+from tkinter import *
+root = Tk() 							# Create the root (base) window 
+w = Label(root, text="Hello, world!") 	# Create a label with words
+w.pack() 								# Put the label into the window
+root.mainloop() 						# Start the event loop
